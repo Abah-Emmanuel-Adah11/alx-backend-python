3986e75e50bb35ca54058917f41cd540adc77361
Abah-Emmanuel-Adah11 <abahemmanueladah11@gmail.com>
2024-03-18 14:49:58 -0700

    Updates

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,17 +1,22 @@
 #!/usr/bin/env python3
-"""
-Let's execute multiple coroutines at the same time with async
-"""
+""" Import wait_random from the previous python file that you’ve written and
+    write an async routine called wait_n that takes in 2 int arguments:
+    max_delay and n. You will spawn wait_random n times with the specified
+    max_delay. wait_n should return the list of all the delays (float values).
+    The list of the delays should be in ascending order without using sort()
+    because of concurrency. """
 import asyncio
 from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    """
-    wait_n function
-    """
-    list_float: List[float] = []
+    """ Let's execute multiple coroutines at the same time with async  """
+    delays: List[float] = []
+    all_delays: List[float] = []
     for i in range(n):
-        list_float.append(await wait_random(max_delay))
-    return sorted(list_float)
+        delays.append(wait_random(max_delay))
+    for delay in asyncio.as_completed(delays):
+        earliest_result = await delay
+        all_delays.append(earliest_result)
+    return all_delays
